f26a47684d2e38fb7f9aa1589b83937f4755dbea
GitHub Action <action@github.com>
2020-07-09 21:02:05 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/inxilpro/testintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.inxilpro.testintellijplugin.services
+
+import com.github.inxilpro.testintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/inxilpro/testintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/inxilpro/testintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.inxilpro.testintellijplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.inxilpro.testintellijplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
